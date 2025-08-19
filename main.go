481400151b770907099d2c72399bf1d0327c8999
0xull <mrikehchukwuka@gmail.com>
2025-08-19 08:54:04 +0100

    refactor: instead of just reading 8bytes and dumping it on the standard out; still read 8bytes, but only dump it after a newline (\n).

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -14,13 +16,23 @@ func main() {
 	defer file.Close()
 
 	buf := make([]byte, 8)
-
+	var current strings.Builder
 	for {
 		n, err := file.Read(buf)
 		if err != nil {
-			break
+			if err == io.EOF {
+				break
+			}
+			log.Fatalf("failed to read file: %v", err)
 		}
 
-		fmt.Printf("read: %s\n", string(buf[:n]))
+		current.Write(buf[:n])
+		s := strings.Split(current.String(), "\n")
+		if len(s) == 2 {
+			fmt.Printf("read: %s\n", s[0])
+			current.Reset()
+			current.WriteString(s[1])
+		}
 	}
+	fmt.Printf("read: %s\n", current.String())
 }
