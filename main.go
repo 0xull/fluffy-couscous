e9286bc4da26893abec7f244552b095460752dfe
0xull <mrikehchukwuka@gmail.com>
2025-08-21 08:47:34 +0100

    refactor: moved the chunked reading into a goroutine, syncing the standard out display of read messages with a channel

--- main.go
+++ main.go
@@ -13,26 +13,39 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed top open file: %v", err)
 	}
-	defer file.Close()
+	
+	for line := range getLineConnection(file) {
+		fmt.Printf("read: %s\n", line)
+	}
+}
 
-	buf := make([]byte, 8)
-	var current strings.Builder
-	for {
-		n, err := file.Read(buf)
-		if err != nil {
-			if err == io.EOF {
+func getLineConnection(f io.ReadCloser) <-chan string {
+	c := make(chan string)
+
+	go func(f io.ReadCloser, c chan<- string) {
+		defer f.Close()
+
+		buf := make([]byte, 8)
+		var current strings.Builder
+		for {
+			n, err := f.Read(buf)
+			if err != nil {
+				if current.String() != "" {
+					c <- current.String()
+				}
+				close(c)
 				break
 			}
-			log.Fatalf("failed to read file: %v", err)
-		}
 
-		current.Write(buf[:n])
-		s := strings.Split(current.String(), "\n")
-		if len(s) == 2 {
-			fmt.Printf("read: %s\n", s[0])
-			current.Reset()
-			current.WriteString(s[1])
+			current.Write(buf[:n])
+			s := strings.Split(current.String(), "\n")
+			if len(s) == 2 {
+				c <- s[0]
+				current.Reset()
+				current.WriteString(s[1])
+			}
 		}
-	}
-	fmt.Printf("read: %s\n", current.String())
+	}(f, c)
+
+	return c
 }
